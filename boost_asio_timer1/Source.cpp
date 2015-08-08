4e19dfb8bc5c25f9add6c7a73b62b0b09cc0ee6f
Morteza Khodadadian <khodadadian.morteza@gmail.com>
2015-08-08 09:06:46 +0430

    timer 2 async

--- boost_asio_timer1/Source.cpp
+++ boost_asio_timer1/Source.cpp
@@ -5,14 +5,19 @@
 #include <boost\date_time\posix_time\posix_time.hpp>
 #include <iostream>
 
+void print(const boost::system::error_code &)
+{
+    std::cout << "print() called" << std::endl;
+}
+
 int main()
 {
     boost::asio::io_service io_service;
     boost::asio::deadline_timer t(io_service, boost::posix_time::seconds(5));
 
-    t.wait();
+    t.async_wait(&print);
 
-    std::cout << "Hello !" << std::endl;
+    io_service.run();
 
     return 0;
 }
